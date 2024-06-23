48879730290bddce4f6dcbb6e1df5c3791a17960
CyberVortex098 <158811621+CyberVortex098@users.noreply.github.com>
2024-06-23 04:17:40 -0700

    Add files via upload

---
+++ TEST_code.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,1);
+  //digitalWrite(9, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
